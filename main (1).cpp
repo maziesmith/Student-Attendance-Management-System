8b31741992234f1f0d46c79a44d39921e60e69b9
nadahalwagy <35423023+nadahalwagy@users.noreply.github.com>
2018-04-03 21:50:01 +0200

    Add files via upload

---
+++ main (1).cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <fstream>
+using namespace std ;
+void main()
+{
+
+	ifstream in ("in.txt");// Declaring the Input File
+	ofstream out ("out.txt");//Declaring the Output File 
+}
